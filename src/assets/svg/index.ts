6c014525f423c3ed51129a5684a0687308ddf6e0
owunr <sjmskm@gmail.com>
2024-05-07 11:38:52 +0900

    add icon svg with colors: sky, green, red, yellow

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,6 @@
+import IconSky from "./sky.svg";
+import IconGreen from "./green.svg";
+import IconRed from "./red.svg";
+import IconYellow from "./yellow.svg";
+
+export { IconSky, IconGreen, IconRed, IconYellow };
