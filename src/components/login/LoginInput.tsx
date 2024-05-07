01920480e53d2fc17a7ca1042da45220af21c357
owunr <sjmskm@gmail.com>
2024-05-07 22:12:54 +0900

    change ID to username

--- src/components/login/LoginInput.tsx
+++ src/components/login/LoginInput.tsx
@@ -23,12 +23,12 @@ const LoginInput = () => {
               htmlFor="email"
               className="block text-sm font-medium text-gray-900"
             >
-              ID
+              User name
             </label>
             <div className="mt-2">
               <input
                 // id="id"
-                name="id"
+                name="name"
                 type="text"
                 required
                 className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-sky-500 text-sm leading-6"
