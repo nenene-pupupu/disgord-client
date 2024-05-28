21545c3c495c7409f0b3b77d1ad34faf48b08b8d
owunr <sjmskm@gmail.com>
2024-05-28 16:57:58 +0900

    empty channel message

--- src/components/chat/ChatRoomList.tsx
+++ src/components/chat/ChatRoomList.tsx
@@ -51,50 +51,65 @@ const ChatRoomList = () => {
         </Tooltip>
       </div>
 
-      <ul role="list" className="divide-y divide-gray-100 overflow-y-scroll">
-        {chatrooms.map((chatRoom) => (
-          <li
-            key={chatRoom.id}
-            className={
-              "flex items-center gap-x-4 py-5 px-2 rounded-md "
-              // (chatRoom.focus && "bg-gray-100")
-            }
-          >
-            <div className="flex items-center w-40 gap-x-4">
-              <img
-                className="h-10 w-10 flex-none rounded-lg bg-gray-50"
-                src={IconSky}
-                alt=""
-              />
-              <div className="min-w-0 flex-auto flex items-center gap-2">
-                <p className={"text-sm font-semibold leading-6 text-gray-900"}>
-                  {chatRoom.name}
-                </p>
-                {chatRoom.password && (
-                  <IoLockClosed className="text-gray-400" />
-                )}
+      <ul
+        role="list"
+        className="divide-y divide-gray-100 overflow-y-scrol w-60"
+      >
+        {chatrooms.length === 0 ? (
+          <>
+            <p className="text-gray-400">
+              No channels here yet
+              <br />
+              Be the first to create one!
+            </p>
+          </>
+        ) : (
+          chatrooms.map((chatRoom) => (
+            <li
+              key={chatRoom.id}
+              className={
+                "flex items-center gap-x-4 py-5 px-2 rounded-md "
+                // (chatRoom.focus && "bg-gray-100")
+              }
+            >
+              <div className="flex items-center w-40 gap-x-4">
+                <img
+                  className="h-10 w-10 flex-none rounded-lg bg-gray-50"
+                  src={IconSky}
+                  alt=""
+                />
+                <div className="min-w-0 flex-auto flex items-center gap-2">
+                  <p
+                    className={"text-sm font-semibold leading-6 text-gray-900"}
+                  >
+                    {chatRoom.name}
+                  </p>
+                  {chatRoom.password && (
+                    <IoLockClosed className="text-gray-400" />
+                  )}
+                </div>
               </div>
-            </div>
-            <div className="flex-1 flex justify-end">
-              {chatRoom.ownerId && (
-                <button
-                  onClick={() => {
-                    setType("modify");
-                    setTarget(chatRoom.id);
-                    setOpenModal(true);
-                  }}
-                >
-                  <IoCreateOutline className="h-6 w-6 text-gray-400 font-thin" />
-                </button>
-              )}
-              <Tooltip message="Enter Room">
-                <button>
-                  <MdChevronRight className="h-6 w-6 text-gray-400 font-thin" />
-                </button>
-              </Tooltip>
-            </div>
-          </li>
-        ))}
+              <div className="flex-1 flex justify-end">
+                {chatRoom.ownerId && (
+                  <button
+                    onClick={() => {
+                      setType("modify");
+                      setTarget(chatRoom.id);
+                      setOpenModal(true);
+                    }}
+                  >
+                    <IoCreateOutline className="h-6 w-6 text-gray-400 font-thin" />
+                  </button>
+                )}
+                <Tooltip message="Enter Room">
+                  <button>
+                    <MdChevronRight className="h-6 w-6 text-gray-400 font-thin" />
+                  </button>
+                </Tooltip>
+              </div>
+            </li>
+          ))
+        )}
       </ul>
     </div>
   );
