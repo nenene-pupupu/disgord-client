e5d59a9bd3a450d77758a37a97b0ae18d038b899
leesoeul <thdmfrhdwn@naver.com>
2024-05-31 16:48:08 +0900

    translate tooltip

--- src/components/common/Tooltip.tsx
+++ src/components/common/Tooltip.tsx
@@ -8,7 +8,7 @@ const Tooltip = ({
   return (
     <div className="group relative flex">
       {children}
-      <span className="absolute -top-8 left-[50%] -translate-x-[50%]  scale-0 rounded bg-gray-400 p-2 text-xs text-white whitespace-nowrap group-hover:scale-100">
+      <span className="absolute -top-7 left-[50%] -translate-x-[70%]  scale-0 rounded bg-gray-400 p-2 text-xs text-white whitespace-nowrap group-hover:scale-90">
         {message}
       </span>
     </div>
