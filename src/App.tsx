13e8f87425c24e3861a480dcb0340d065db9abf8
owunr <sjmskm@gmail.com>
2024-06-04 15:55:26 +0900

    remove sign button if already signed in

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { IconSky } from "@/assets/svg";
+import { useAtomValue } from "jotai";
 import { Link } from "react-router-dom";
+import { tokenAtom } from "./atoms/AuthAtom";
 
 function App() {
+  const token = useAtomValue(tokenAtom);
+
   return (
     <div className="w-full h-full flex flex-col justify-center items-center gap-8">
       <img src={IconSky} className="w-24 h-24" />
@@ -19,11 +23,13 @@ function App() {
             Explore chatrooms
           </button>
         </Link>
-        <Link to="/login">
-          <button className="px-6 py-4 text-xl bg-sky-400 text-white rounded-md border-2 border-sky-400 font-semibold">
-            Sign up for Free
-          </button>
-        </Link>
+        {!token && (
+          <Link to="/login">
+            <button className="px-6 py-4 text-xl bg-sky-400 text-white rounded-md border-2 border-sky-400 font-semibold">
+              Sign up for Free
+            </button>
+          </Link>
+        )}
       </div>
     </div>
   );
